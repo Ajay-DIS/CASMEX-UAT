78365d1fab524ca0d743b40dcc716becdad34fed
Ajay-191883 <ajaysuper11@gmail.com>
2023-02-23 12:02:26 +0530

    third stash change

--- src/app/banks-routing/transaction-criteria-modal/transaction-criteria-modal.ts
+++ src/app/banks-routing/transaction-criteria-modal/transaction-criteria-modal.ts
@@ -13,82 +13,48 @@ import { DynamicDialogRef } from "primeng/dynamicdialog";
   styleUrls: ["./transaction-criteria-modal.css"],
 })
 export class TransactionCriteriaModal {
-  // constructor(public ref: DynamicDialogRef, private fb: FormBuilder) {}
+  showError: boolean = false;
+  txnCriteriaRangeForm: FormGroup;
+  txnCriteriaRange: FormArray;
 
-  // txnAmountRangesForm = this.fb.group({
-  //   txnAmountRanges: new FormArray([
-  //     this.fb.group({
-  //       from: [2, Validators.required],
-  //       to: [5, Validators.required],
-  //     }),
-  //     this.fb.group({
-  //       from: [10, Validators.required],
-  //       to: [20, Validators.required],
-  //     }),
-  //   ]),
-  // });
-
-  // showError: boolean = false;
-
-  // get txnAmountRanges() {
-  //   return this.txnAmountRangesForm.controls["txnAmountRanges"] as FormArray;
-  // }
-
-  // get txnAmountRangeForm() {
-  //   return this.txnAmountRanges.controls as unknown as FormGroup;
-  // }
-
-  // ngOnInit() {
-  //   console.log("::range", this.txnAmountRanges);
-  // }
-
-  // addTxnRange() {
-  //   if (this.txnAmountRanges.length >= 10) {
-  //     this.showError = true;
-  //     setTimeout(() => {
-  //       this.showError = false;
-  //     }, 1500);
-  //   } else {
-  //     const txnAmountRangeForm = this.fb.group({
-  //       from: [null, Validators.required],
-  //       to: [null, Validators.required],
-  //     });
-
-  //     this.txnAmountRanges.push(txnAmountRangeForm);
-  //   }
-  // }
-
-  // saveTxnAmountRanges() {
-  //   console.log(this.txnAmountRangesForm.value);
-  // }
-
-  // dsffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-
-  orderForm: FormGroup;
-  items: FormArray;
-
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private ref: DynamicDialogRef) {
+    this.txnCriteriaRangeForm = new FormGroup({
+      txnCriteriaRange: new FormArray([]),
+    });
+  }
 
   ngOnInit() {
-    this.orderForm = new FormGroup({
-      items: new FormArray([]),
-    });
+    this.addTxnCriteriaRange();
   }
 
-  createItem(): FormGroup {
+  createTxnCriteriaRange(): FormGroup {
     return this.formBuilder.group({
-      name: "",
-      description: "",
-      price: "",
+      from: null,
+      to: null,
     });
   }
 
-  addItem(): void {
-    this.items = this.orderForm.get("items") as FormArray;
-    this.items.push(this.createItem());
+  addTxnCriteriaRange(): void {
+    if (this.txnCriteriaRange?.length >= 10) {
+      this.showError = true;
+      setTimeout(() => {
+        this.showError = false;
+      }, 1500);
+    } else {
+      this.txnCriteriaRange = this.txnCriteriaRangeForm.get(
+        "txnCriteriaRange"
+      ) as FormArray;
+      this.txnCriteriaRange.push(this.createTxnCriteriaRange());
+    }
   }
 
-  save() {
-    console.log(this.orderForm.value);
+  saveTxnCriteriaRanges() {
+    console.log(this.txnCriteriaRangeForm.value);
+  }
+  
+  closeModal() {
+    this.saveTxnCriteriaRanges()
+    this.ref.close(this.txnCriteriaRangeForm.value);
+
   }
 }
