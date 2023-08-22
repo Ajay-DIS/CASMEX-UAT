235e7cb2fd02eeabadc816b9096688dcffeb9daa
sureshhanu1219 <hanumanth.s@distinctinfotech.com>
2023-08-22 18:58:08 +0530

    customer search changes

--- src/app/customer-profile/customer-profile.component.ts
+++ src/app/customer-profile/customer-profile.component.ts
@@ -81,88 +81,7 @@ export class CustomerProfileComponent implements OnInit {
     { name: "Corporate", code: "Corporate" },
   ];
 
-  searchCriteriaApiData = [
-    // {
-    //   id: 6,
-    //   applicationName: "Casmex Core",
-    //   formName: "Customer Profile Individual",
-    //   moduleName: "Remittance",
-    //   fieldName: "firstName",
-    //   displayName: "First Name",
-    //   operators: null,
-    //   orderID: 1,
-    //   sqlQueries: null,
-    //   iSMandatory: "N",
-    //   dependency: "N",
-    //   status: "A",
-    //   criteriaType: "text",
-    //   masterDataDependency: null,
-    //   hqlMasterData: null,
-    //   hqlDependency: null,
-    //   hqlDependencyData: null,
-    //   createdDate: null,
-    // },
-    // {
-    //   id: 7,
-    //   applicationName: "Casmex Core",
-    //   formName: "Customer Profile Individual",
-    //   moduleName: "Remittance",
-    //   fieldName: "phoneNumber",
-    //   displayName:"Phone Number",
-    //   operators: null,
-    //   orderID: 2,
-    //   sqlQueries: null,
-    //   iSMandatory: "N",
-    //   dependency: "N",
-    //   status: "A",
-    //   criteriaType: "number",
-    //   masterDataDependency: null,
-    //   hqlMasterData: null,
-    //   hqlDependency: null,
-    //   hqlDependencyData: null,
-    //   createdDate: null,
-    // },
-    // {
-    //   id: 8,
-    //   applicationName: "Casmex Core",
-    //   formName: "Customer Profile Individual",
-    //   moduleName: "Remittance",
-    //   fieldName: "lastName",
-    //   displayName: "Last Name",
-    //   operators: null,
-    //   orderID: 3,
-    //   sqlQueries: null,
-    //   iSMandatory: "N",
-    //   dependency: "N",
-    //   status: "A",
-    //   criteriaType: "text",
-    //   masterDataDependency: null,
-    //   hqlMasterData: null,
-    //   hqlDependency: null,
-    //   hqlDependencyData: null,
-    //   createdDate: null,
-    // },
-    // {
-    //   id: 10,
-    //   applicationName: "Casmex Core",
-    //   formName: "Customer Profile Individual",
-    //   moduleName: "Remittance",
-    //   fieldName: "dateOfBirth",
-    //   displayName: "Date Of Birth",
-    //   operators: null,
-    //   orderID: 4,
-    //   sqlQueries: null,
-    //   iSMandatory: "N",
-    //   dependency: "N",
-    //   status: "A",
-    //   criteriaType: "date",
-    //   masterDataDependency: null,
-    //   hqlMasterData: null,
-    //   hqlDependency: null,
-    //   hqlDependencyData: null,
-    //   createdDate: null,
-    // },
-  ];
+  searchCriteriaApiData = [];
 
   searchCriteriaOptions = [];
   searchCriteria = [];
@@ -252,6 +171,8 @@ export class CustomerProfileComponent implements OnInit {
     this.criteriaMap = "NA";
     this.searchCriteria = [];
     this.currentCriteriaValue = null;
+    this.currentCriteriaKey = "Customer ID = ";
+    this.currentCriteriaMapKey = "id = ";
     this.searchCriteriaMap = [];
     this.getApiDataForsearchCriteria();
     this.getCustomerListData();
@@ -341,6 +262,9 @@ export class CustomerProfileComponent implements OnInit {
 
     this.getCustomerListData();
     this.currentCriteriaValue = null;
+    this.customerFieldType = null;
+    this.currentCriteriaKey = "Customer ID = ";
+    this.currentCriteriaMapKey = "id = ";
   }
 
   getCustomerListData() {
@@ -377,6 +301,7 @@ export class CustomerProfileComponent implements OnInit {
             this.cols = this.colsIND;
             this.customerData = res.data.CmIndividualCustomerDetails;
           }
+          // this.totalRecords = res.data.PaginationDetails.totalCount;
           this.customerCode = res.customerCode?.map((code) => {
             if (code) return { label: code, value: code };
           });
@@ -543,5 +468,18 @@ export class CustomerProfileComponent implements OnInit {
 
   fieldFilterOptions(field: any): [] {
     return this[field];
+  }
+
+
+  clearCriteria(){
+    this.criteriaMap = "NA";
+    this.searchCriteria = [];
+    this.currentCriteriaValue = null;
+    this.currentCriteriaKey = "Customer ID = ";
+    this.currentCriteriaMapKey = "id = ";
+    this.searchCriteriaMap = [];
+    this.getApiDataForsearchCriteria();
+    this.getCustomerListData();
+    this.customerFieldType = null;
   }
 }
